8ee053d3bbacadfdc1c2dae219877bc6717815fd
Secrethan <rladlfgks422@gmail.com>
2023-02-15 23:55:09 +0900

    items 테이블 생성

---
+++ hobby_store/src/main/webapp/sql/han.sql
@@ -0,0 +1,91 @@
+--상품 정보 테이블 시작--
+create table items(
+items_num number not null,
+cate_num varchar2(3) not null,
+itmes_name varchar2(30) not null,
+itmes_price number(10) not null,
+itmes_quantity number(5) not null,
+itmes_photo1 varchar2(150) not null,
+itmes_photo2 varchar2(150),
+itmes_photo3 varchar2(150),
+itmes_content clob not null,
+itmes_hit number not null,
+itmes_date date default sysdate not null,
+itmes_mdate date,
+mem_num number not null,
+constraint items_pk primary key (items_num),
+constraint items_fk1 foreign key (mem_num) references member (mem_num)
+);
+
+create sequence items_seq;
+--상품 정보 테이블 끝 --
+
+--상품 카테고리 테이블 시작--
+create table itmes_cate(
+cate_num number not null,
+cate_parent number(1),
+cate_name varchar2(30) not null,
+constraint itmes_cate_pk primary key (cate_num)
+);
+
+create sequence items_cate_seq;
+--상품 카테고리 테이블 끝--
+
+--상품 좋아요 테이블 시작--
+create table items_fav (
+fav_num number not null,
+items_num number not null,
+mem_num number not null,
+constraint items_fav_pk primary key(fav_num),
+constraint items_fav_fk1 foreign key(items_num) references items (items_num),
+constraint items_fav_fk2 foreign key(mem_num) references member (mem_num)
+);
+
+create sequence items_fav_seq;
+--상품 좋아요 테이블 끝--
+
+--상품 후기 저장 테이블 시작--
+create table items_reply(
+reply_num number not null,
+items_num number not null,
+mem_num number not null,
+reply_content varchar2(900) not null,
+reply_date date default sysdate not null,
+reply_mdate date,
+reply_photo varchar2(150),
+constraint items_reply primary key(reply_num),
+constraint items_reply_fk1 foreign key(items_num) references items (items_num),
+constraint items_reply_fk2 foreign key(mem_num) references member (mem_num)
+);
+
+create sequence items_reply_seq;
+--상품 후기 저장 테이블 끝--
+
+--상품 후기 좋아요 테이블 시작--
+create table items_reply_fav (
+fav_num number not null,
+items_num number not null,
+mem_num number not null,
+constraint items_reply_fav_pk primary key(fav_num),
+constraint items_reply_fav_fk1 foreign key(items_num) references items (items_num),
+constraint items_reply_fav_fk2 foreign key(mem_num) references member (mem_num)
+);
+
+create sequence items_reply_fav_seq;
+--상품 후기 좋아요 테이블 끝--
+
+--상품 후기 별점 테이블 시작--
+create table items_star(
+star_num number not null,
+items_num number not null,
+mem_num number not null,
+star_auth number(1) not null,
+constraint items_star_pk primary key(star_num),
+constraint items_star_fk1 foreign key(items_num) references items (items_num),
+constraint items_star_fk2 foreign key(mem_num) references member (mem_num)
+);
+
+create sequence items_star_seq;
+--상품 후기 별점 테이블 끝--
+
+
