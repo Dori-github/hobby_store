d08565d832597ba83f5eee94af395ae286280dab
phseon <88360147+phseon@users.noreply.github.com>
2023-02-16 17:43:12 +0900

    장바구니 Controller, Service 추가: 메서드 생성, 데이터 처리

---
+++ hobby_store/src/main/java/kr/spring/cart/controller/CartController.java
@@ -0,0 +1,50 @@
+package kr.spring.cart.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import kr.spring.cart.controller.CartController;
+import kr.spring.cart.service.CartService;
+import kr.spring.cart.vo.CourseCartVO;
+
+@Controller
+public class CartController {//메서드 생성, 데이터 처리
+	@Autowired
+	private CartService cartService;
+	
+	private static final Logger logger =
+			LoggerFactory.getLogger(
+					CartController.class);
+	
+	//=====장바구니 목록=====//
+	@RequestMapping("/cart/list.do")
+	public ModelAndView getList() {	
+		
+			//글의 총 개수
+			int count = cartService.getCartCount();
+			
+			//목록 호출
+			List<CourseCartVO> list = null;
+			if(count > 0) {
+				list = cartService.getCartList(count);
+			}
+			
+			ModelAndView mav = new ModelAndView();
+			//뷰 이름 설정
+			mav.setViewName("selectList");
+			//데이터 저장
+			mav.addObject("count", count);
+			
+			logger.debug("???!!???");
+			
+			return mav;
+	}		
+}
