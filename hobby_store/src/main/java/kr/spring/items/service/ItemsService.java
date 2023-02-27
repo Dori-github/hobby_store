9442f4087387a6f23c6148e039633a39d295a158
Secrethan <rladlfgks422@gmail.com>
2023-02-28 02:14:16 +0900

    김일한 | 상품 등록 가능(추가 수정 예정)

--- hobby_store/src/main/java/kr/spring/items/service/ItemsService.java
+++ hobby_store/src/main/java/kr/spring/items/service/ItemsService.java
@@ -3,6 +3,8 @@ package kr.spring.items.service;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.annotations.Select;
+
 import kr.spring.items.vo.ItemsVO;
 
 public interface ItemsService {
@@ -10,6 +12,20 @@ public interface ItemsService {
 	public void insertItems(ItemsVO items);
 	
 	//상품 목록
-	public List<ItemsVO> selectList(Map<String, Object> map);
-	public int selectRowCount(Map<String, Object> map); 
+	public List<ItemsVO> selectItemsList(Map<String, Object> map);
+	//상품 카운트
+	public int selectItemsCount(Map<String, Object> map); 
+	//상품 선택
+	public ItemsVO selectItems(Integer items_num);
+	//상품 수정
+	public void updateItems(ItemsVO itemsVO);
+	//상품 삭제
+	public void deleteItems(ItemsVO itemsVO);
+	
+	//부모 카테고리 
+	public List<ItemsVO> selectCate1();
+	
+	public List<ItemsVO> selectCate2(Integer cate_num);
+
+
 }
--- hobby_store/src/main/java/kr/spring/items/vo/ItemsVO.java
+++ hobby_store/src/main/java/kr/spring/items/vo/ItemsVO.java
@@ -1,111 +1,198 @@
 package kr.spring.items.vo;
 
+import java.io.IOException;
 import java.sql.Date;
+import java.util.Arrays;
+
+import org.springframework.web.multipart.MultipartFile;
 
 public class ItemsVO {
 	private int items_num;
-	private String cate_num;
+	private int cate_num;
+	private int cate_parent;
+	private String cate_name;
 	private String items_name;
-	private int price;
-	private int quantity;
-	private String photo1;
-	private String photo2;
-	private String photo3;
+	private int items_price;
+	private int items_quantity;
+	private int status;
+	private byte[] items_photo1;
+	private byte[] items_photo2;
+	private byte[] items_photo3;
+	private String items_photo_name1;
+	private String items_photo_name2;
+	private String items_photo_name3;
 	private Date reg_date;
 	private Date mdate;
 	private String items_content;
-	private int hit;
+	private int items_hit;
 	private int mem_num;
-	
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public int getCate_parent() {
+		return cate_parent;
+	}
+
+	public void setCate_parent(int cate_parent) {
+		this.cate_parent = cate_parent;
+	}
+
+	public String getCate_name() {
+		return cate_name;
+	}
+
+	public void setCate_name(String cate_name) {
+		this.cate_name = cate_name;
+	}
+
 	public int getItems_num() {
 		return items_num;
 	}
+
 	public void setItems_num(int items_num) {
 		this.items_num = items_num;
 	}
-	public String getCate_num() {
+
+	public int getCate_num() {
 		return cate_num;
 	}
-	public void setCate_num(String cate_num) {
+
+	public void setCate_num(int cate_num) {
 		this.cate_num = cate_num;
 	}
+
 	public String getItems_name() {
 		return items_name;
 	}
+
 	public void setItems_name(String items_name) {
 		this.items_name = items_name;
 	}
-	public int getPrice() {
-		return price;
+
+	public int getItems_price() {
+		return items_price;
 	}
-	public void setPrice(int price) {
-		this.price = price;
+
+	public void setItems_price(int items_price) {
+		this.items_price = items_price;
 	}
-	public int getQuantity() {
-		return quantity;
+
+	public int getItems_quantity() {
+		return items_quantity;
 	}
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
+
+	public void setItems_quantity(int items_quantity) {
+		this.items_quantity = items_quantity;
 	}
-	public String getPhoto1() {
-		return photo1;
-	}
-	public void setPhoto1(String photo1) {
-		this.photo1 = photo1;
-	}
-	public String getPhoto2() {
-		return photo2;
-	}
-	public void setPhoto2(String photo2) {
-		this.photo2 = photo2;
-	}
-	public String getPhoto3() {
-		return photo3;
-	}
-	public void setPhoto3(String photo3) {
-		this.photo3 = photo3;
-	}
+
 	public Date getReg_date() {
 		return reg_date;
 	}
+
 	public void setReg_date(Date reg_date) {
 		this.reg_date = reg_date;
 	}
+
 	public Date getMdate() {
 		return mdate;
 	}
+
 	public void setMdate(Date mdate) {
 		this.mdate = mdate;
 	}
+
 	public String getItems_content() {
 		return items_content;
 	}
+
 	public void setItems_content(String items_content) {
 		this.items_content = items_content;
 	}
-	public int getHit() {
-		return hit;
-	}
-	public void setHit(int hit) {
-		this.hit = hit;
-	}
+
 	public int getMem_num() {
 		return mem_num;
 	}
+
 	public void setMem_num(int mem_num) {
 		this.mem_num = mem_num;
 	}
-	
-	@Override
-	public String toString() {
-		return "ItemsVO [items_num=" + items_num + ", cate_num=" + cate_num + ", items_name=" + items_name + ", price="
-				+ price + ", quantity=" + quantity + ", photo1=" + photo1 + ", photo2=" + photo2 + ", photo3=" + photo3
-				+ ", reg_date=" + reg_date + ", mdate=" + mdate + ", items_content=" + items_content + ", hit=" + hit
-				+ ", mem_num=" + mem_num + "]";
+
+	public byte[] getItems_photo1() {
+		return items_photo1;
 	}
-	
-	
-	
-	
-		
+
+	public void setItems_photo1(byte[] items_photo1) {
+		this.items_photo1 = items_photo1;
+	}
+
+	public byte[] getItems_photo2() {
+		return items_photo2;
+	}
+
+	public void setItems_photo2(byte[] items_photo2) {
+		this.items_photo2 = items_photo2;
+	}
+
+	public byte[] getItems_photo3() {
+		return items_photo3;
+	}
+
+	public void setItems_photo3(byte[] items_photo3) {
+		this.items_photo3 = items_photo3;
+	}
+
+	public String getItems_photo_name1() {
+		return items_photo_name1;
+	}
+
+	public void setItems_photo_name1(String items_photo_name1) {
+		this.items_photo_name1 = items_photo_name1;
+	}
+
+	public String getItems_photo_name2() {
+		return items_photo_name2;
+	}
+
+	public void setItems_photo_name2(String items_photo_name2) {
+		this.items_photo_name2 = items_photo_name2;
+	}
+
+	public String getItems_photo_name3() {
+		return items_photo_name3;
+	}
+
+	public void setItems_photo_name3(String items_photo_name3) {
+		this.items_photo_name3 = items_photo_name3;
+	}
+
+	public int getItems_hit() {
+		return items_hit;
+	}
+
+	public void setItems_hit(int items_hit) {
+		this.items_hit = items_hit;
+	}
+
+	// 업로드 파일 처리
+	public void setUpload1(MultipartFile upload1) throws IOException {
+		// MultipartFile -> byte[] 변환
+		setItems_photo1(upload1.getBytes());
+	}
+
+	public void setUpload2(MultipartFile upload2) throws IOException {
+		// MultipartFile -> byte[] 변환
+		setItems_photo2(upload2.getBytes());
+	}
+
+	public void setUpload3(MultipartFile upload3) throws IOException {
+		// MultipartFile -> byte[] 변환
+		setItems_photo3(upload3.getBytes());
+	}
+
 }
