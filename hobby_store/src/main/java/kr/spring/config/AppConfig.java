0bc2e5ad249972135177063f38b4328f5f08f09a
shkim <shkim_114@naver.com>
2023-02-28 15:38:07 +0900

    김소희 이메일 AppConfig, pom 수정

--- hobby_store/src/main/java/kr/spring/config/AppConfig.java
+++ hobby_store/src/main/java/kr/spring/config/AppConfig.java
@@ -1,7 +1,10 @@
 package kr.spring.config;
 
+import java.util.Properties;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
@@ -74,6 +77,25 @@ public class AppConfig implements WebMvcConfigurer{
 		tilesViewResolver.setViewClass(TilesView.class);
 		return tilesViewResolver;
 	}
+	//이메일 설정 
+	@Bean
+    public JavaMailSenderImpl javaMailSenderImpl() {
+    	Properties prop = new Properties();
+    	prop.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+    	prop.put("mail.smtp.starttls.enable", "true");
+    	prop.put("mail.transport.protocol", "smtp");
+    	prop.put("mail.smtp.auth", "true");
+    	prop.put("mail.debug", "true");
+    	
+    	JavaMailSenderImpl javaMail = new JavaMailSenderImpl();
+    	javaMail.setHost("smtp.gmail.com");
+    	javaMail.setPort(587);
+    	javaMail.setDefaultEncoding("utf-8");
+    	javaMail.setUsername("shkim38822@gmail.com");
+    	javaMail.setPassword("lklyuhhgbywbygkn");
+    	javaMail.setJavaMailProperties(prop);
+    	return javaMail;
+    }
 }
 
 
